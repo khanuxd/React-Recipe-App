8792a89ca7d340179f418cfd9d675305d957a437
Khan Fayjul <khanuxd@gmail.com>
2021-05-31 10:34:46 +0300

    responsive nav issue fixed

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container, Image } from 'react-bootstrap';
 import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
-
+import logo from '../../assests/logo.png';
 
 const Navigation = () => {
     return (
         <Navbar collapseOnSelect bg="light" expand="lg">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <IndexLinkContainer to="/">
-                        <Nav.Link>Home</Nav.Link>
-                    </IndexLinkContainer>
-                    <LinkContainer to="/recipes">
-                        <Nav.Link>Recipes</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/add-new-recipe">
-                        <Nav.Link>Add New Recipe</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/about">
-                        <Nav.Link>About</Nav.Link>
-                    </LinkContainer>
-                </Nav >
-            </Navbar.Collapse >
+            <Container>
+                <Navbar.Brand href="/"><Image src={logo} alt="Recipe" /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <IndexLinkContainer to="/">
+                            <Nav.Link>Home</Nav.Link>
+                        </IndexLinkContainer>
+                        <LinkContainer to="/recipes">
+                            <Nav.Link>Recipes</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/add-new-recipe">
+                            <Nav.Link>Add New Recipe</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/about">
+                            <Nav.Link>About</Nav.Link>
+                        </LinkContainer>
+                    </Nav >
+                </Navbar.Collapse >
+            </Container>
         </Navbar >
     );
 };
